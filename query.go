d928d1754ae8d6f543b4e73d56e75cafc8dc3034
Brad Berger <brad@bradb.net>
2017-09-22 17:00:41 +0200

    Adds mechanism to allow mock queries

--- query.go
+++ query.go
@@ -1,19 +1,94 @@
 package aedstorm
 
 import (
+	"errors"
 	"reflect"
+	"sync"
+
+	"golang.org/x/net/context"
 
 	"google.golang.org/appengine/datastore"
 )
 
+var (
+	nextResultData  interface{}
+	nextResultError error
+	nextResultKeys  []*datastore.Key
+	mu              sync.Mutex
+)
+
+// SetMockQueryResult sets the query GetAll() result to be the values given
+func SetMockQueryResult(data interface{}, err error, keys []*datastore.Key) {
+	mu.Lock()
+	defer mu.Unlock()
+	nextResultData = data
+	nextResultError = err
+	nextResultKeys = keys
+}
+
+func clearNextResult() {
+	mu.Lock()
+	defer mu.Unlock()
+	nextResultData = nil
+	nextResultError = nil
+	nextResultKeys = nil
+}
+
+func hasNextResult() bool {
+	mu.Lock()
+	defer mu.Unlock()
+	return nextResultData != nil || nextResultError != nil || nextResultKeys != nil
+}
+
+// Query is a struct which implements a subset of the "datastore.Query" interface and is mockable
+type Query struct {
+	entity string
+	dq     *datastore.Query
+}
+
+// Filter implements the "datastore.Query".Filter interface
+func (q *Query) Filter(filterStr string, value interface{}) *Query {
+	if q.dq == nil {
+		q.dq = datastore.NewQuery(q.entity)
+	}
+	q.dq = q.dq.Filter(filterStr, value)
+	return q
+}
+
+// Count matches the "datastore.Query".Count interface
+func (q *Query) Count(ctx context.Context) (int, error) {
+	if q.dq == nil {
+		q.dq = datastore.NewQuery(q.entity)
+	}
+	return q.dq.Count(ctx)
+}
+
+// GetAll matches the "datastore.Query".GetAll interface
+func (q *Query) GetAll(ctx context.Context, out interface{}) ([]*datastore.Key, error) {
+
+	// For purposes of mocking, this allows a one-time return value to be preset in advance
+	if hasNextResult() {
+		defer clearNextResult()
+		if err := Copy(nextResultData, out); err != nil {
+			return nil, err
+		}
+		return nextResultKeys, nextResultError
+	}
+
+	if q.dq == nil {
+		q.dq = datastore.NewQuery(q.entity)
+	}
+	return q.dq.GetAll(ctx, out)
+}
+
 // NewQuery returns a new query based off the type of m. If m implements the EntityName interface, it uses
 // that for an entity name, otherwise it uses the name of the struct itself
-func NewQuery(m interface{}) *datastore.Query {
+func NewQuery(m interface{}) *Query {
 	entityKind, err := getEntityName(m)
 	if err != nil {
 		panic(err)
 	}
-	return datastore.NewQuery(entityKind)
+	return &Query{entity: entityKind}
 }
 
 // getEntityName returns the name of a struct type based on the EntityName interface value
@@ -38,4 +113,25 @@ func getEntityName(m interface{}) (string, error) {
 		}
 	}
 	return entityKind, nil
+}
+
+// Copy copies one interface into the other doing type checking to make sure
+// it's safe. If it cannot be copied, an error is returned.
+func Copy(srcVal interface{}, dstVal interface{}) error {
+	curEl := reflect.ValueOf(srcVal)
+	if curEl.Kind() == reflect.Ptr {
+		curEl = curEl.Elem()
+	}
+	dstEl := reflect.ValueOf(dstVal)
+	if dstEl.Kind() == reflect.Ptr {
+		dstEl = dstEl.Elem()
+	}
+	if !dstEl.CanSet() {
+		return errors.New("Destination value type is invalid")
+	}
+	if !curEl.Type().AssignableTo(dstEl.Type()) {
+		return errors.New("Cannot assign value to destination")
+	}
+	dstEl.Set(curEl)
+	return nil
 }
